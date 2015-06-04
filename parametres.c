c97ae85bbd53deb8c759f66cf0af6681f16673f3
amaixent <alice.maixent@gmail.com>
2015-06-04 15:37:00 +0200

    paramètres de niveau

---
+++ parametres.c
@@ -0,0 +1,48 @@
+// niveau 2  
+
+  perso[0] = createRectangle(4, 4, -60, 80, 40, 68, 1.4);
+  perso[1] = createRectangle(10, 2, -56, 80, 58, 48, 2);
+  perso[2] = createRectangle(4, 8, -54, 80, 52, 80, 1);
+
+
+  obs[0] = createRectangle(150, 50, -110, -30, 0, 0, 0);
+  obs[1] = createRectangle(150, 50, 60, -30, 0, 0, 0);
+
+  obs[2] = createRectangle(20, 120, -60, -20, 0, 0, 0);
+  obs[3] = createRectangle(80, 20, -60, 0, 0, 0, 0);
+  obs[4] = createRectangle(40, 12, -40, 0, 0, 0, 0);
+  obs[5] = createRectangle(4, 4, -28, 20, 0, 0, 0);
+  obs[6] = createRectangle(8, 8, -12, 0, 0, 0, 0);
+  obs[7] = createRectangle(4, 8, -4, 16, 0, 0, 0);
+  obs[8] = createRectangle(48, 56, 4, 0, 0, 0, 0);
+  obs[9] = createRectangle(8, 8, 20, 48, 0, 0, 0);
+  obs[10] = createRectangle(8, 8, 36, 60, 0, 0, 0);
+  obs[11] = createRectangle(20, 16, 44, 60, 0, 0, 0);
+  obs[12] = createRectangle(24, 52, -28, 72, 0, 0, 0);
+  obs[13] = createRectangle(12, 36, -28, 60, 0, 0, 0);
+  obs[14] = createRectangle(12, 12, -20, 48, 0, 0, 0);
+  obs[15] = createRectangle(8, 8, -28, 52, 0, 0, 0);
+  obs[16] = createRectangle(60, 120, -60, 96, 0, 0, 0);
+
+
+// niveau 3
+
+  perso[0] = createRectangle(10, 2, -80, 40, -26, 96, 2);
+  perso[1] = createRectangle(4, 4, -78, 40, 64, 104, 1.3);
+  perso[2] = createRectangle(6, 4, -74, 40, 36, 104, 1);
+
+  obs[0] = createRectangle(150, 50, -130, -30, 0, 0, 0);
+  obs[1] = createRectangle(150, 50, 80, -30, 0, 0, 0);
+  obs[2] = createRectangle(20, 40, -80, -20, 0, 0, 0);
+  obs[3] = createRectangle(52, 28, -8, -20, 0, 0, 0);
+  obs[4] = createRectangle(4, 12, -20, 4, 0, 0, 0);
+  obs[5] = createRectangle(8, 8, 20, 20, 0, 0, 0);
+  obs[6] = createRectangle(8, 36, -80, 32, 0, 0, 0);
+  obs[7] = createRectangle(4, 24, 16, 52, 0, 0, 0);
+  obs[8] = createRectangle(8, 32, 36, 72, 0, 0, 0);
+  obs[9] = createRectangle(4, 20, -48, 64, 0, 0, 0);
+  obs[10] = createRectangle(4, 16, -32, 92, 0, 0, 0);
+  obs[11] = createRectangle(4, 12, 32, 100, 0, 0, 0);
+  obs[12] = createRectangle(4, 12, 60, 100, 0, 0, 0);
+
+
